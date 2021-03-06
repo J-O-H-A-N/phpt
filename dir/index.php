fc62348fd3c0d966a0586dba886257a2544d7fde
J-O-H-A-N <dasankush9284@gmail.com>
2021-03-06 13:41:31 +0530

    update

--- dir/index.php
+++ dir/index.php
@@ -1,6 +1,15 @@
 <?php
-    echo "Hello World!";
-    $var="moshi moshi";
-    echo $var;
+    function SumMarks($MarksArr)
+    {
+        $sum = 0;
+        foreach ($MarksArr as $value) {
+            $sum += $value;
+        }
+        return $value;
+    }
 
+    $RohanMarks = [34, 45, 67];
+    echo "The sum of marks of Rohan out of 300: ". SumMarks($RohanMarks). "<br>";
+    $HarryMarks = [90, 98, 99];
+    echo "The sum of marks of Harry out of 300: ". SumMarks($HarryMarks);
 ?>
--- dir/keywords.php
+++ dir/keywords.php
@@ -1,4 +1,105 @@
 <?php
-strrev($string);
-substr_replace($string, "replace", string[index]);
+
+
+
+. // operator to concatenate strings
+
+data types:
+    int,
+    float,
+    object,
+    array, $arr = array("ankush", "mukesh", "mohit");
+    boolean, $var = false; $var1 = true;
+    null,
+    string;
+
+Operators:
+1. Arithmetic Operators
+    Addition: $a + $b;
+    Substraction: $a - $b;
+    Division: $a / $b;
+    Multiplication: $a * $b;
+    Modulus: $a % $b;
+    Exponential: $a ** $b;
+
+2. Assignment Operators
+    $a = $b;
+    $a += $b: $a = $a + $b;
+    $a -= $b: $a = $a - $b;
+    $a /= $b: $a = $a / $b;
+    $a *= $b: $a = $a * $b;
+    $a %= $b: $a = $a % $b;
+
+3. Comparision Operators
+    $x == $y: if $x == $y then var_dump($x == $y) is TRUE else otherwise;
+    $x < $y: if $x < $y then var_dump($x < $y) is TRUE else otherwise;
+    $x > $y: if $x > $y then var_dump($x > $y) is TRUE else otherwise;
+    $x >= $y: if $x >= $y then var_dump($x >= $y) is TRUE else otherwise;
+    $x <= $y: if $x <= $y then var_dump($x <= $y) is TRUE else otherwise;
+    $x <> $y: if $x <> $y then var_dump($x <> $y) is TRUE else otherwise;
+
+4. Logical Operators
+    $a && $b: if $a is TRUE && $b is False then var_dump($a && $b) is False;
+    $a || $b: if $a is TRUE || $b is False then var_dump($a || $b) is TRUE;
+    !$a: if $a is TRUE then var_dump(!$a) is False;
+
+Conditional Statements:
+    1. If-Else Statements
+    2. If-Elseif Statements
+    3. Switch Statements
+        switch(age)
+        {
+            case 12:
+                echo "You are 12 years old";
+                break;
+            case 45: 
+                echo "You are 45 years old";
+                break;
+            default: 
+                echo "Invalid age";
+                break;
+            }
+4.While loop
+    $i =0;
+    while($i>5)
+    {
+        echo $i; echo "<br>";
+        i++;
+    }
+5.For loop
+    for(initilizatin; condition; updation){
+        statement;
+    }
+6.Do while loop
+    Do
+    {
+        echo $i;
+        $i++;
+    } while ($a <= 10);
+7.Foreach loop
+    foreach
+    ($variable as $key => $value) {
+        # code...
+    }
+    /*
+    $arr = array("bananas", "apple");
+    foreach($arr as $value) {
+        echo "$value <br>";
+    }
+    */
+
+echo is the only thing that is not 
+
+strlen($var); // length of the given string
+strrev($string); //reverse the string
+substr_replace("before", "After", $string); // replace the string
+str_repeat($string, number_of_repetition); // repeat the string n times
+strpos($string , "string"); // gives the index of the "string" in $string
+
+var_dump(); // tells the data type and value of the variable
+count(); //function to count elements in array
+ltrim("    this is a string    "); // will remove whitespaces from left
+rtrim("    this is a string    "); // will remove whitespaces from right
+
+
 ?>
